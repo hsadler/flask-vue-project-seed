d2e38ee8663396e75a695aef38d2485279389677
Harry Sadler <harrysadlermusic@gmail.com>
2018-03-18 16:40:58 -0700

    add navigation to Vue client, add .dockerignore

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
+    <Navigation></Navigation>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navigation from '@/components/Navigation'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navigation
+  }
 }
 </script>
 
---
+++ app/src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navigation-container">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  props: {}
+}
+</script>
+
+<style scoped>
+  div.navigation-container {
+    padding: 30px;
+  }
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+</style>
