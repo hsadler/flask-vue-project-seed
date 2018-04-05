d93de513d0b3a0adeb84db92073e10a91d5c851b
Harry Sadler <harrysadlermusic@gmail.com>
2018-04-04 23:47:59 -0700

    update shell scripts for real

--- shell-scripts/up-dev.sh
+++ shell-scripts/up-dev.sh
@@ -1,4 +1,3 @@
 
-cd .. && \
 docker-compose -f docker-compose.dev.yml up --build --force-recreate \
 --remove-orphans --abort-on-container-exit
--- shell-scripts/up-prod.sh
+++ shell-scripts/up-prod.sh
@@ -1,4 +1,3 @@
 
-cd .. && \
 docker-compose -f docker-compose.yml up --build --force-recreate \
 --remove-orphans --abort-on-container-exit
