5341866a154ef8b0288a9245adbac61620629d70
Harry Sadler <harrysadlermusic@gmail.com>
2018-04-18 01:08:37 -0700

    partially implement BaseDataObject class

--- app/server/data_object/base_data_object.py
+++ app/server/data_object/base_data_object.py
@@ -10,32 +10,87 @@ class BaseDataObject(metaclass=ABCMeta):
 	"""
 
 
-	def __init__(self, prop_dict={}):
-		pass
+	def __init__(self, prop_dict, db_driver, cache_driver):
+		self.db_driver = db_driver
+		self.cache_driver = cache_driver
+		self.state = prop_dict
 
 
 	########## BASE METHODS ##########
 
-	@classmethod
-	def create(cls, prop_dict={}):
-		pass
 
 	@classmethod
-	def find_many(cls, prop_dict={}, limit=None):
-		pass
+	def create(cls, prop_dict={}, db_driver=None, cache_driver=None):
+
+		db_driver, cache_driver = cls.__get_drivers(db_driver, cache_driver)
+
+		return cls(
+			prop_dict=prop_dict,
+			db_driver=db_driver,
+			cache_driver=cache_driver
+		)
+
 
 	@classmethod
-	def find_one(cls, prop_dict={}):
-		pass
+	def find_many(
+		cls,
+		prop_dict={},
+		limit=None,
+		db_driver=None,
+		cache_driver=None
+	):
+
+		db_driver, cache_driver = cls.__get_drivers(db_driver, cache_driver)
+
+		records = db_driver.find_by_fields(
+			table_name=cls.TABLE_NAME,
+			where_props=prop_dict,
+			limit=limit
+		)
+
+		instances = [
+			cls(
+				prop_dict=record,
+				db_driver=db_driver,
+				cache_driver=cache_driver
+			)
+			for record in records
+		]
+
+		return instances
+
+
+	@classmethod
+	def find_one(cls, prop_dict={}, db_driver=None, cache_driver=None):
+
+		instance_list = cls.find_many(
+			prop_dict=prop_dict,
+			limit=1,
+			db_driver=db_driver,
+			cache_driver=cache_driver
+		)
+
+		if len(instance_list) > 0:
+			return instance_list[0]
+		else:
+			return None
+
 
 	def get_prop(self, prop_name):
-		pass
+		return self.state[prop_name]
+
 
 	def set_prop(self, prop_name, prop_value):
-		pass
+		if prop_name in self.state:
+			self.state[prop_name] = prop_value
+			return True
+		else:
+			return False
+
 
 	def save(self):
 		pass
+
 
 	def delete(self):
 		pass
@@ -49,6 +104,18 @@ class BaseDataObject(metaclass=ABCMeta):
 
 
 	########## PRIVATE HELPERS ##########
+
+
+	__get_drivers(db_driver=None, cache_driver=None):
+
+		db_driver = db_driver if db_driver is not None \
+		else cls.DEFAULT_DB_DRIVER
+
+		cache_driver = cache_driver if cache_driver is not None \
+		else cls.DEFAULT_CACHE_DRIVER
+
+		return db_driver, cache_driver
+
 
 	__get_prop_names():
 		pass
--- app/server/data_object/example_data_oject.py
+++ app/server/data_object/example_data_oject.py
@@ -2,6 +2,7 @@
 # Example Data Object
 
 from dataobject.base_data_object import BaseDataObject
+from data_store.database_driver.mysql_driver import MySqlDriver
 
 
 class ExampleDataObject(BaseDataObject):
@@ -10,4 +11,10 @@ class ExampleDataObject(BaseDataObject):
 	"""
 
 
-	# ExampleDataObject specific methods go here..
+	TABLE_NAME = 'example'
+	DEFAULT_DB_DRIVER = MySqlDriver()
+	DEFAULT_CACHE_DRIVER = None
+
+
+	# Subclass specific methods go here...
+
