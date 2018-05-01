007bc6e0d724ce43ccb6c638eb35838828b6bfe6
Harry Sadler <harrypatricksadler@gmail.com>
2018-05-01 14:17:06 -0700

    add a docstring

--- app/server/api/web/wall_messages_api.py
+++ app/server/api/web/wall_messages_api.py
@@ -2,13 +2,16 @@
 from flask import Blueprint, jsonify
 
 from service.wall_messages import WallMessages
-
-
 web_wall_messages_api = Blueprint('web_wall_messages_api', __name__)
 
 
 @web_wall_messages_api.route('/get-all')
 def get_all():
+	"""
+	Get all wall messages.
+
+	"""
+
 	wms = WallMessages.get_all()
 	response = [ wm.to_dict() for wm in wms ]
 	return jsonify(response)
