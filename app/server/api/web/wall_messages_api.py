54edeadff803dd0f5a4dc22e2ea42d1adbda2c34
Harry Sadler <harrypatricksadler@gmail.com>
2018-05-01 19:06:18 -0700

    add wall_message api method for adding a message

--- app/server/api/web/wall_messages_api.py
+++ app/server/api/web/wall_messages_api.py
@@ -5,7 +5,7 @@ from service.wall_messages import WallMessages
 web_wall_messages_api = Blueprint('web_wall_messages_api', __name__)
 
 
-@web_wall_messages_api.route('/get-all')
+@web_wall_messages_api.route('/get-all', methods=['GET'])
 def get_all():
 	"""
 	Get all wall messages.
@@ -15,3 +15,19 @@ def get_all():
 	wms = WallMessages.get_all()
 	response = [ wm.to_dict() for wm in wms ]
 	return jsonify(response)
+
+
+@web_wall_messages_api.route('/add-message', methods=['POST'])
+def add_message():
+	"""
+	Add a wall message.
+
+	"""
+
+	message_body = str(request.form['body'])
+	message_attribution = str(request.form['attribution'])
+	wm = WallMessages.add_message(
+		message_body=message_body,
+		message_attribution=message_attribution
+	)
+	return jsonify(wm)
