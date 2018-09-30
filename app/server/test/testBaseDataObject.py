6e7a42bda05e85b4acde585aae11815a449a1517
Harry Sadler <harrysadlermusic@gmail.com>
2018-09-29 23:04:41 -0700

    reorganize data access tests, stub out new tests in base data object test script

--- app/server/test/testBaseDataObject.py
+++ app/server/test/testBaseDataObject.py
@@ -22,11 +22,22 @@ mysql_driver = MySqlDriver(
 	database_name=config.MYSQL_DB_NAME
 )
 
+TABLE_NAME = 'test_user'
+
+
+######## DELETE TEST TABLE ########
+
+
+drop_table_query = """
+	DROP TABLE IF EXISTS {0}
+""".format(TABLE_NAME)
+query_result = mysql_driver.query_bind(query_string=drop_table_query)
+ppp('result of drop table query:', query_result)
+
 
 ######## CREATE TEST TABLE ########
 
 
-TABLE_NAME = 'test_user'
 create_table_query = """
 	CREATE TABLE IF NOT EXISTS {0} (
 		uuid VARCHAR(32) PRIMARY KEY,
@@ -51,7 +62,7 @@ class TestUserDataObject(BaseDataObject):
 	DEFAULT_CACHE_TTL = 1
 
 
-######## TEST DATA OBJECT INTERFACE ########
+######## TEST DATA OBJECT CRUD INTERFACE ########
 
 
 # test 'create' method
@@ -136,24 +147,26 @@ t.should_be_equal(
 )
 
 
-# test 'get_prop' method
-DO_name = found_test_user_DO.get_prop(prop_name='name')
-ppp("'get_prop' method name:", DO_name)
-t.should_be_equal(
-	expected=user_data['name'],
-	actual=DO_name
-)
+# test 'find_by_uuids' method
+# @classmethod
+# def find_by_uuids(
+# 	cls,
+# 	uuids=[],
+# 	db_driver_class=None,
+# 	cache_driver_class=None,
+# 	cache_ttl=None
+# ):
 
 
-# test 'set_prop' method
-new_age = 101
-set_prop_res = found_test_user_DO.set_prop(prop_name='age', prop_value=new_age)
-ppp("'set_prop' res:", set_prop_res)
-ppp("test user data object after 'set_prop':", found_test_user_DO.to_dict())
-t.should_be_equal(
-	expected=new_age,
-	actual=found_test_user_DO.get_prop(prop_name='age')
-)
+# test 'find_by_uuid' method
+# @classmethod
+# def find_by_uuid(
+# 	cls,
+# 	uuid,
+# 	db_driver_class=None,
+# 	cache_driver_class=None,
+# 	cache_ttl=None
+# ):
 
 
 # test 'delete' method
@@ -163,6 +176,140 @@ t.should_be_equal(
 	expected=bool,
 	actual=type(delete_res)
 )
+
+
+########## TEST DATA OBJECT DATA ACCESS INTERFACE ##########
+
+
+user_data = {
+	'name': 'Frank',
+	'age': 32,
+	'admin': 1
+}
+test_user_DO = TestUserDataObject.create(prop_dict=user_data)
+test_user_DO.save()
+
+
+# test 'get_prop' method
+test_user_name = test_user_DO.get_prop(prop_name='name')
+ppp("'get_prop' method name property:", test_user_name)
+t.should_be_equal(
+	expected=user_data['name'],
+	actual=test_user_name
+)
+
+
+# test 'set_prop' method
+new_age = 101
+set_prop_res = test_user_DO.set_prop(prop_name='age', prop_value=new_age)
+ppp("'set_prop' res:", set_prop_res)
+ppp("test user data object after 'set_prop':", test_user_DO.to_dict())
+t.should_be_equal(
+	expected=new_age,
+	actual=test_user_DO.get_prop(prop_name='age')
+)
+
+# test 'get_metadata' method
+test_user_created_ts = test_user_DO.get_metadata(
+	TestUserDataObject.CREATED_TS_METADATA
+)
+ppp("'get_metadata' method 'created_ts' property:", test_user_created_ts)
+t.should_be_equal(
+	expected=type(test_user_created_ts),
+	actual=int
+)
+
+
+# test 'set_metadata' method (metadata is managed automatically within the
+# dataobject so this method shouldn't ever need to be used)
+new_updated_ts = 666
+set_metadata_res = test_user_DO.set_metadata(
+	metadata_name=TestUserDataObject.UPDATED_TS_METADATA,
+	metadata_value=new_updated_ts
+)
+ppp("'set_metadata' res:", set_metadata_res)
+ppp("test user data object after 'set_metadata':", test_user_DO.to_dict())
+t.should_be_equal(
+	expected=new_updated_ts,
+	actual=test_user_DO.get_metadata(
+		metadata_name=TestUserDataObject.UPDATED_TS_METADATA
+	)
+)
+
+
+########## TEST DATA OBJECT SERIALIZATION, DATABASE, CACHE INTERFACE ##########
+
+
+# @classmethod
+# def load_database_records(
+# 	cls,
+# 	records,
+# 	db_driver_class,
+# 	cache_driver_class,
+# 	records_are_new=False
+# ):
+
+
+# @classmethod
+# def get_drivers(cls, db_driver_class=None, cache_driver_class=None):
+
+
+# @classmethod
+# def load_from_database_by_uuids(
+# 	cls,
+# 	uuids,
+# 	db_driver_class,
+# 	cache_driver_class
+# ):
+
+
+# @classmethod
+# def load_from_database_by_uuid(
+# 	cls,
+# 	uuid,
+# 	db_driver_class,
+# 	cache_driver_class
+# ):
+
+
+# @classmethod
+# def construct_cache_key(cls, uuid):
+
+
+# def set_to_cache(self, ttl=None):
+
+
+# @classmethod
+# def set_batch_to_cache(
+# 	cls,
+# 	dataobjects,
+# 	db_driver_class,
+# 	cache_driver_class,
+# 	ttl=None,
+# ):
+
+
+# def delete_from_cache(self):
+
+
+# @classmethod
+# def delete_batch_from_cache(cls, dataobjects=[]):
+
+
+# @classmethod
+# def load_from_cache_by_uuids(
+# 	cls,
+# 	uuids,
+# 	db_driver_class,
+# 	cache_driver_class
+# ):
+
+
+# @classmethod
+# def load_from_cache_by_uuid(cls, uuid, db_driver_class, cache_driver_class):
+
+
+########## TEST DATA OBJECT UTILITY INTERFACE ##########
 
 
 ######## DELETE TEST TABLE ########
