251bfc846a1362e4d9f6a2b2cf9fb7763531a822
Harry Sadler <harrysadlermusic@gmail.com>
2022-01-23 19:58:07 +0100

    add vue3 app initialized files for migration, update TODOs

--- app/server/main.py
+++ app/server/main.py
@@ -1,14 +1,14 @@
 
 from flask import (
     Flask,
-    render_template,
+    # render_template,
 )
 
 # init Flask app instance
 app = Flask(
     __name__,
-    static_folder='../client/dist/static',
-    template_folder='../client/dist'
+    # static_folder='../client/dist/static',
+    # template_folder='../client/dist'
 )
 
 
@@ -29,10 +29,10 @@ app.register_blueprint(web_wall_messages_api, url_prefix='/api/wall-messages')
 
 
 # serve index for non-API calls
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template('index.html')
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return render_template('index.html')
 
 
 # run the app if executed as main file to python interpreter
