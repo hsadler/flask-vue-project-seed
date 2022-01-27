a14daca332ba2e475362aeac4e1e7cf8c170f055
Harry Sadler <harrysadlermusic@gmail.com>
2022-01-27 23:09:23 +0100

    add commented styles to component

--- app/client/src/components/Navigation.vue
+++ app/client/src/components/Navigation.vue
@@ -12,4 +12,17 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<!--
+<style scoped lang="scss">
+  div.navigation-container {
+    padding: 30px;
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+</style>
+-->
