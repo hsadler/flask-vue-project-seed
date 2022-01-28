14a3b72af1306a54c0d2716fc4dc49c8010d6c7b
Harry Sadler <harrysadlermusic@gmail.com>
2022-01-28 19:30:44 +0100

    add flask-cors python dep, configure cors for json api server, update client calls to use new port for x-origin json api calls

--- app/client/src/services/httpService.js
+++ app/client/src/services/httpService.js
@@ -1,14 +1,16 @@
 
 import axios from 'axios'
 
+const HOST = 'http://localhost:8000'
+
 class HttpService {
   get (url) {
-    return axios.get(url).then(res => {
+    return axios.get(HOST + url).then(res => {
       return res.data
     })
   }
   post (url, options) {
-    return axios.post(url, options).then(res => {
+    return axios.post(HOST + url, options).then(res => {
       return res.data
     })
   }
--- app/server/main.py
+++ app/server/main.py
@@ -1,15 +1,17 @@
 
 from flask import (
     Flask,
-    jsonify
+)
+from flask_cors import (
+    CORS,
 )
 
-# init Flask app instance
+# init Flask app instance and configure CORS
 app = Flask(__name__)
-
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # ping route for testing
-@app.route('/api/ping', methods=['GET'])
+@app.route('/ping', methods=['GET'])
 def handle_ping():
     from api.web.wall_messages_api import get_all
     from utils.print import ppp
@@ -18,11 +20,9 @@ def handle_ping():
     ppp(all)
     return all
 
-
 # register api routes
 from api.web.wall_messages_api import web_wall_messages_api
 app.register_blueprint(web_wall_messages_api, url_prefix='/api/wall-messages')
-
 
 # run the app if executed as main file to python interpreter
 if __name__ == '__main__':
